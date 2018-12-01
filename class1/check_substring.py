cbfafcce10e2b1834dbf2164bc1beeec492da2d2
jijithjimmy <jijithjimmy@gmail.com>
2018-12-01 16:22:55 +0530

    substring in a string

---
+++ class1/check_substring.py
@@ -0,0 +1,8 @@
+sports="cricket,football,basketball,baseball"
+
+favourite_sport=input("Please enter your favourite sport :-")
+
+if favourite_sport in sports:
+    print("I found my favourite sport here !!!")
+else:
+    print("Couldn't find my favourite sport here !!!")
