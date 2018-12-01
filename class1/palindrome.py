08087ce37d5fc592452c6ec694a0ffb71df8dde2
jijithjimmy <jijithjimmy@gmail.com>
2018-12-01 16:15:50 +0530

    palindrome program

---
+++ class1/palindrome.py
@@ -0,0 +1,8 @@
+name=input("Please enter a string now :-")
+
+reverse_name=name[-1: :-1]
+
+if name == reverse_name:
+    print("Ënterred string is a palindrome!!!")
+else:
+    print("Not a palindrome")
--- first_hello.py
+++
@@ -1,2 +0,0 @@
-name="Jijith Jimmy"
-print(name)
