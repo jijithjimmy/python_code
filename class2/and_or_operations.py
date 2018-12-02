d17e928809344bbf5b19e8b524c1623afe1b7d2c
jijithjimmy <jijithjimmy@gmail.com>
2018-12-02 16:08:14 +0530

    and or operators

---
+++ class2/and_or_operations.py
@@ -0,0 +1,10 @@
+age=int(input("Please enter your age :-"))
+salary=int(input("Please enter your salary:- "))
+
+if age > 27 :
+    if salary > 50000 :
+        print("GOOD TO MARRY !!!")
+    else :
+        print("You have low salary !!!and not good to marry ")
+else:
+    print("You dont have enough age to get marry !!! SORRY....")
---
+++ class2/and_or_operations_correct.py
@@ -0,0 +1,7 @@
+age=int(input("Please enter your age :-"))
+salary=int(input("Please enter your salary:- "))
+
+if age > 27 and salary > 50000 :
+    print("GOOD TO MARRY !!!")
+else:
+    print("Not good to marry")
