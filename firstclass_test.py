b98eac19221d6ef6981d21e6b46d778ae848f108
jijithjimmy <jijithjimmy@gmail.com>
2018-12-02 15:10:05 +0530

    test program

---
+++ firstclass_test.py
@@ -0,0 +1,8 @@
+random_value=int(input("Please enter a random number :-"))
+random_sequenc=input("Please enter a random sequence :-")
+
+#total_length=len(random_sequenc)
+
+data=int(random_sequenc[0: :2])
+final_product=random_value*data
+print(final_product)
