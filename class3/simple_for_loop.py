580b119df5d71c730a21dc15671e27fafbe18df9
jijithjimmy <jijithjimmy@gmail.com>
2018-12-08 15:03:23 +0530

    simple for loop

--- class3/simple_for_loop.py
+++ class3/simple_for_loop.py
@@ -1,2 +1,5 @@
-for counter in range(5):
-    print("Hello World !!!")
+name=input("Please enter a name :-")
+no_of_times=int(input("Please enter how many times you need to print:-"))
+
+for counter in range(0,no_of_times):
+    print(name)
