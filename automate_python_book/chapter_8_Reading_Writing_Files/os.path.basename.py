5ff7bb3b0bea44dacc4ef9611142e75a41929a92
Jimmy <jjimmy@BANL16b00baa5.local>
2019-08-04 10:29:23 +0530

    learning

---
+++ automate_python_book/chapter_8_Reading_Writing_Files/os.path.basename.py
@@ -0,0 +1,7 @@
+import os
+
+print(os.path.basename('/Users/jjimmy/Desktop/SBI/PANCARD/3.pdf'))
+
+print(os.path.dirname('/Users/jjimmy/Desktop/SBI/PANCARD/3.pdf'))
+
+print(os.path.dirname('/Users/jjimmy/Desktop/SBI/PANCARD/3.pdf'),os.path.basename('/Users/jjimmy/Desktop/SBI/PANCARD/3.pdf'))
---
+++ automate_python_book/chapter_8_Reading_Writing_Files/os.path.split.py
@@ -0,0 +1,3 @@
+import os
+
+print(os.path.split('/Users/jjimmy/Desktop/SBI/PANCARD/3.pdf'))
