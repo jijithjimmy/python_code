20a68a35a1e1ed7515c5adb2e45dd2698d46ee23
Jimmy <jjimmy@BANL16b00baa5.local>
2019-08-04 12:21:01 +0530

    learning

---
+++ automate_python_book/chapter_8_Reading_Writing_Files/file_read.py
@@ -0,0 +1,7 @@
+infile=open('/Users/jjimmy/Desktop/workspace/python_code/automate_python_book/chumma.txt','r')
+
+file_content=infile.read()
+
+infile.close()
+
+print(file_content)
---
+++ automate_python_book/chapter_8_Reading_Writing_Files/file_write.py
@@ -0,0 +1,8 @@
+outfile=open('another_chumma.txt','w')
+
+outfile.write('indecom'+'\n')
+outfile.write('intuit'+'\n')
+
+outfile.close()
+
+print()
